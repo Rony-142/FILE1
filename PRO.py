9b9e84d3c8420ffef619d27aaf390d8892e15b96
Rony-142 <132200368+Rony-142@users.noreply.github.com>
2023-05-01 00:11:48 +0600

    Create PRO.py

---
+++ PRO.py
@@ -0,0 +1,14 @@
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pip install requests
+pip install mechanize
+pip install bs4
+pip install rich
+pkg install git
+rm-rf FILE1
+git clone https://github.com/Rony-142/FILE1.git
+cd FILE1
+git pull 
+python PRO.py
